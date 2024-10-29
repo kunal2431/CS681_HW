680bd13b2a282330fd44c28956c99b20d5402ff1
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2024-10-28 21:35:56 -0400

    Add files via upload

---
+++ HW-06-2/src/main/umbcs681/VolprimesFac/RunnableCancellablePrimeFactorizer.java
@@ -0,0 +1,54 @@
+package umbcs681.VolprimesFac;
+
+//import java.util.concurrent.locks.Lock;
+//import java.util.concurrent.locks.ReentrantLock;
+
+public class RunnableCancellablePrimeFactorizer extends RunnablePrimeFactorizer {
+	volatile boolean done = false;
+	//private ReentrantLock lock = new ReentrantLock();
+	
+	public RunnableCancellablePrimeFactorizer(long dividend, long from, long to) {
+		super(dividend, from, to);
+	}
+	
+	public void setDone(){
+		done = true;
+	}
+
+	public void generatePrimeFactors() {
+		long divisor = from;
+		while( dividend != 1 && divisor <= to ){
+			if (done) {
+				System.out.println("Stopped generating prime factors.");
+				this.factors.clear();
+				break;
+			}
+			if( divisor > 2 && isEven(divisor)) {
+				divisor++;
+				continue;
+			}
+			if(dividend % divisor == 0) {
+				factors.add(divisor);
+				dividend /= divisor;
+			}else {
+				if(divisor==2){ divisor++; }
+				else{ divisor += 2; }
+			}
+
+		}
+	}
+
+	public static void main(String[] args) {
+		RunnableCancellablePrimeFactorizer gen = new RunnableCancellablePrimeFactorizer(36, 2, (long)Math.sqrt(36));
+		Thread thread = new Thread(gen);
+		thread.start();
+		gen.setDone();
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		gen.getPrimeFactors().forEach( (Long prime)-> System.out.print(prime + ", ") );
+		System.out.println("\n" + gen.getPrimeFactors().size() + " prime factors are found.");
+	}
+}
