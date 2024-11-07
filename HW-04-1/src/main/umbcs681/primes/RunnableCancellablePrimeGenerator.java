5969270a1ccd12548a83e1dca249033473b622da
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2024-11-07 12:52:18 -0500

    Remove-main()

--- HW-04-1/src/main/umbcs681/primes/RunnableCancellablePrimeGenerator.java
+++ HW-04-1/src/main/umbcs681/primes/RunnableCancellablePrimeGenerator.java
@@ -40,18 +40,5 @@ public class RunnableCancellablePrimeGenerator extends RunnablePrimeGenerator {
 			}
 		}
 	}
-
-	public static void main(String[] args) {
-		RunnableCancellablePrimeGenerator gen = new RunnableCancellablePrimeGenerator(1,100);
-		Thread thread = new Thread(gen);
-		thread.start();
-		gen.setDone();
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		gen.getPrimes().forEach( (Long prime)-> System.out.print(prime + ", ") );
-		System.out.println("\n" + gen.getPrimes().size() + " prime numbers are found.");
-	}
+	
 }
--- HW-07-1/src/main/umbcs681/stepprimes/RunnableCancellablePrimeGenerator.java
+++ HW-07-1/src/main/umbcs681/stepprimes/RunnableCancellablePrimeGenerator.java
@@ -47,18 +47,4 @@ public class RunnableCancellablePrimeGenerator extends RunnablePrimeGenerator {
 		}
 	}
 
-	public static void main(String[] args) {
-		RunnableCancellablePrimeGenerator gen = new RunnableCancellablePrimeGenerator(1,100);
-		Thread thread = new Thread(gen);
-		thread.start();
-		gen.setDone();
-		thread.interrupt();
-		try {
-			thread.join();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		gen.getPrimes().forEach( (Long prime)-> System.out.print(prime + ", ") );
-		System.out.println("\n" + gen.getPrimes().size() + " prime numbers are found.");
-	}
 }
