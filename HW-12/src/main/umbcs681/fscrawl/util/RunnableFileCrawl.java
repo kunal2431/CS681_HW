fd958d329c34de09354154eff5e1faf1283f07d5
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2024-12-19 17:10:26 -0800

    review changes

--- HW-12/src/main/umbcs681/fscrawl/util/RunnableFileCrawl.java
+++ HW-12/src/main/umbcs681/fscrawl/util/RunnableFileCrawl.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.time.LocalDateTime;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class RunnableFileCrawl implements Runnable{
 
@@ -15,8 +14,6 @@ public class RunnableFileCrawl implements Runnable{
 
     private AtomicBoolean done = new AtomicBoolean(false);
 
-    private ReentrantReadWriteLock rwLock = new ReentrantReadWriteLock();
-
     public RunnableFileCrawl(Directory root_dir, FileCrawlingVisitor visitor){
         this.root_dir = root_dir;
         this.visitor = visitor;
@@ -24,155 +21,47 @@ public class RunnableFileCrawl implements Runnable{
 
     public void setDone(){done.set(true);}
 
-    /*public void run() {
-        while(!done.get()){
-            root_dir.accept(visitor);
-            LinkedList<File> result = visitor.getFiles();
-
-            shared_list.updateAndGet(rootFileList -> {
-                for (File file : result) {
-                    if(done.get()){
-                        System.out.println("Stopped RunnableFileCrawl during traversing");
-                        break;
-                    }
-                    if (!rootFileList.contains(file)) {
-                        rootFileList.add(file);
-                        System.out.println("File crawled: " + file.getName());
-                    }
-                    try{
-                        Thread.sleep(200);
-                    }
-                    catch (InterruptedException e){
-
-                    }
-                }
-                return rootFileList;
-            });
-            try{
-                Thread.sleep(500);
-            }
-            catch (InterruptedException ex){
-                break;
-            }
-        }
-    }*/
-
     public void run() {
-        while (true) {
+        for (int i = 0; i < 1; i++){
             if(done.get()){
                 System.out.println("Stopped RunnableFileCrawl");
                 break;
             }
-            try {
-                fileCrawl(root_dir, visitor);
+/*            try{
+                Thread.sleep(1); // I used sleep() to get intermediate results
+            }
+            catch (InterruptedException ex){}*/
+            root_dir.accept(visitor);
+            LinkedList<File> result = visitor.getFiles();
+            shared_list.updateAndGet(rootFileList -> {
+                for (File file : result) {
+                    if (!rootFileList.contains(file)) {
+                        rootFileList.add(file);
+                    }
+                }
+                return rootFileList;
+            });
+            for(File f: result){
+                System.out.println(f.getName());
+            }
+            try{
+                Thread.sleep(2);
+            }
+            catch (InterruptedException ex){
 
-            } catch (InterruptedException e) {
-                System.out.println("Stopped FileCrawling during Traversing");
+            }
+            if(done.get()){
+                System.out.println("Stopped RunnableFileCrawl");
                 break;
             }
-            finally {
-                LinkedList<File> result = visitor.getFiles();
-                shared_list.updateAndGet(rootFileList -> {
-                    for (File file : result) {
-                        if (!rootFileList.contains(file)) {
-                            rootFileList.add(file);
-                            System.out.println("File crawled: " + file.getName());
-                        }
-                    }
-                    return rootFileList;
-                });
-            }
         }
     }
 
-    public void fileCrawl(Directory rootDir, FSVisitor v) throws InterruptedException {
-        rwLock.readLock().lock();
-        try {
-            v.visit(rootDir);
-            LinkedList<FSElement> children = new LinkedList<>(rootDir.getChildren());
-            for (FSElement e : children) {
-                if (done.get() || Thread.currentThread().isInterrupted()) {throw new InterruptedException();}
-                if(e instanceof Directory){
-                    fileCrawl((Directory) e, v);
-                }
-                else{
-                    e.accept(v);
-                }
-            }
-        } finally {
-            rwLock.readLock().unlock();
-        }
-    }
 
     public static LinkedList<File> getShared_list() {
         return shared_list.get();
     }
 
-   /* public void run() {
-        while (true) {
-            if(done.get()){
-                System.out.println("Stopped RunnableFileCrawl");
-                break;
-            }
-            try {
-                fileCrawl(root_dir, visitor);
-
-            } catch (InterruptedException e) {break;}
-            finally {
-                LinkedList<File> result = visitor.getFiles();
-                shared_list.updateAndGet(rootFileList -> {
-                    for (File file : result) {
-                        if (!rootFileList.contains(file)) {
-                            rootFileList.add(file);
-                            System.out.println("File crawled: " + file.getName());
-                        }
-                    }
-                    return rootFileList;
-                });
-            }
-        }
-    }
-
-    public void fileCrawl(Directory rootDir, FSVisitor v) throws InterruptedException {
-        rwLock.readLock().lock();
-        try {
-            v.visit(rootDir);
-            LinkedList<FSElement> children = new LinkedList<>(rootDir.getChildren());
-            for (FSElement e : children) {
-                if (Thread.currentThread().isInterrupted()) {throw new InterruptedException();}
-                if(e instanceof Directory){
-                    fileCrawl((Directory) e, v);
-                }
-                else{
-                    e.accept(v);
-                }
-            }
-        } finally {
-            rwLock.readLock().unlock();
-        }
-        try {
-            Thread.sleep(3);
-        } catch (InterruptedException ex) {
-            throw new InterruptedException();
-        }
-    }
-
-    public void accept(FSVisitor v) throws InterruptedException {
-        rwLock.readLock().lock();
-        try {
-            if (Thread.currentThread().isInterrupted()) {throw new InterruptedException();}
-            v.visit(this);
-        } finally {
-            rwLock.readLock().unlock();
-        }
-        try{
-            Thread.sleep(3);
-        }
-        catch (InterruptedException ex){}
-    }
-
-
-*/
 
 /*    Alternate Solution:
 
