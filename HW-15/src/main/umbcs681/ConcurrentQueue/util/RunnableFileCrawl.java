00570578b4edff6f5223bdb0c17d476bc48393b8
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2024-12-19 18:02:14 -0800

    review changes

--- HW-15/src/main/umbcs681/ConcurrentQueue/util/RunnableFileCrawl.java
+++ HW-15/src/main/umbcs681/ConcurrentQueue/util/RunnableFileCrawl.java
@@ -22,7 +22,7 @@ public class RunnableFileCrawl implements Runnable{
 
     public void setDone(){done.set(true);}
 
-    /*public void run() {
+    public void run() {
         for(int i = 0; i< 1; i++){
             if(done.get()){
                 System.out.println("Stopped FileCrawlRunnable");
@@ -31,92 +31,21 @@ public class RunnableFileCrawl implements Runnable{
             root_dir.accept(visitor);
             shared_list.addAll(visitor.getFiles());
             try{
-                Thread.sleep(500);
+                Thread.sleep(2);
             }
             catch (InterruptedException ex){
                 break;
             }
-        }
-    }*/
-
-    public void run() {
-        while (true) {
             if(done.get()){
-                System.out.println("Stopped RunnableFileCrawl");
+                System.out.println("Stopped FileCrawlRunnable");
                 break;
-            }
-            try {
-                fileCrawl(root_dir, visitor);
-
-            } catch (InterruptedException e) {
-                System.out.println("Stopped FileCrawlRunnable during Traversing");
-                break;
-            }
-            finally {
-                ConcurrentLinkedQueue<File> result = visitor.getFiles();
-                for (File file : result) {
-                    if (!shared_list.contains(file)) {
-                        shared_list.add(file);
-                        System.out.println("File crawled: " + file.getName());
-                    }
-                }
             }
         }
     }
 
-    public void fileCrawl(Directory rootDir, FSVisitor v) throws InterruptedException {
-            v.visit(rootDir);
-            ConcurrentLinkedQueue<FSElement> children = new ConcurrentLinkedQueue<>(rootDir.getChildren());
-            for (FSElement e : children) {
-                if (done.get() || Thread.currentThread().isInterrupted()) {throw new InterruptedException();}
-                if(e instanceof Directory){
-                    fileCrawl((Directory) e, v);
-                }
-                else{
-                    e.accept(v);
-                }
-            }
-    }
 
     public static ConcurrentLinkedQueue<File> getShared_list() {
         return shared_list;
     }
-
-
-/*    Alternate Solution:
-
-    private LinkedList<File> shared_list1 = new LinkedList<File>();
-
-    public void run() {
-        for(int i = 0; i < 1; i++){
-            if(done.get()){
-                System.out.println("Stopped RunnableFileCrawl");
-                break;
-            }
-            root_dir.accept(visitor);
-            LinkedList<File> result = visitor.getFiles();
-
-            for (File file : result) {
-                if (!shared_list1.contains(file)) {
-                    shared_list1.add(file);
-                    System.out.println("File crawled: " + file.getName());
-
-                }
-            }
-
-            try{
-                Thread.sleep(500);
-            }
-            catch (InterruptedException ex){
-                continue;
-            }
-        }
-    }
-
-    public static LinkedList<File> getShared_list1() {
-        return shared_list1;
-    }
-
-    */
 
 }
