e7a9a35867096db125464cfa13548983e328f038
Kunal.P.Sangurmath <sangurmathkunal@gmail.com>
2024-12-19 17:47:38 -0800

    review changes

--- HW-14/src/main/umbcs681/nestedlock/ReadRunnable.java
+++ HW-14/src/main/umbcs681/nestedlock/ReadRunnable.java
@@ -16,7 +16,7 @@ public class ReadRunnable implements Runnable{
 	}
 
 	public void run(){
-		while(true){
+		for(int i = 0; i < 1; i++){
 			if (done.get()) {
 				System.out.println("Stopped ReadRunnable");
 				break;
@@ -28,7 +28,11 @@ public class ReadRunnable implements Runnable{
 				Thread.sleep(2);
 			}
 			catch (InterruptedException ex){
-				continue;
+
+			}
+			if (done.get()) {
+				System.out.println("Stopped ReadRunnable");
+				break;
 			}
 		}
 
